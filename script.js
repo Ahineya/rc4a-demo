d777690083f4501aac3c6019a1bb4b59c30f6aac
Pavel Evsegneev <pe@newagesol.com>
2017-03-14 23:44:33 +0200

    Replace tabs with spaces

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 ((window) => {
-	const inputElem = document.querySelector(".input");
-	const outputElem = document.querySelector(".output");
-	const keyElem = document.querySelector(".key");
-	const doButton = document.querySelector(".do");
+    const inputElem = document.querySelector(".input");
+    const outputElem = document.querySelector(".output");
+    const keyElem = document.querySelector(".key");
+    const doButton = document.querySelector(".do");
 
-	doButton.addEventListener("click", () => {
-		const text = inputElem.value;
-		keyElem.value = keyElem.value || "" + Math.rand();
-		const key = keyElem.value;
+    doButton.addEventListener("click", () => {
+        const text = inputElem.value;
+        keyElem.value = keyElem.value || "" + Math.rand();
+        const key = keyElem.value;
 
-		outputElem.innerText = window.rc4a(text, key);
-	});
+        outputElem.innerText = window.rc4a(text, key);
+    });
 
 })(window);
